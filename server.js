4f10e8d7d646497d8b800acd01db6a09d2edafce
karthiknerella <nerella.karthik007@gmail.com>
2017-08-16 18:56:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 app.get('article-one', function (req, res) {
   res.sendFile('article 1 is served here');
 });
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 app.get('article-three', function (req, res) {
   res.sendFile('article 3 is served here');
 });
